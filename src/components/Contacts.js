12c682d427b9042fb8fda69e6242c8e7de0c741c
Bavin Edwards <65621465+zerico007@users.noreply.github.com>
2021-03-18 05:26:02 -0500

    links open in a new tab

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,61 +1,76 @@
-import React from 'react';
-import styled from '@emotion/styled';
-import {GitHub, Linkedin, Inbox} from 'react-feather'
+import React from "react";
+import styled from "@emotion/styled";
+import { GitHub, Linkedin, Inbox } from "react-feather";
 
 const ContactBox = styled.div`
-    width: 100px;
-    height: 100px;
-    border-radius: 60px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 10px;
-    margin-top: 40px;
-    box-shadow: 0.3rem 0.3rem 0.3rem 0.5rem lightgrey;
-    transition: all 0.3s ease-in-out;
-    cursor: pointer;
-    background: #78FFF1;
-    color: #361999;
-    border: 2px solid #361999;
-    &:hover {
-        background: #361999;
-        color: #78FFF1;
-        border: 2px solid #78FFF1;
-        transform: scale(1.2);
-    }
+  width: 100px;
+  height: 100px;
+  border-radius: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  margin-top: 40px;
+  box-shadow: 0.3rem 0.3rem 0.3rem 0.5rem lightgrey;
+  transition: all 0.3s ease-in-out;
+  cursor: pointer;
+  background: #78fff1;
+  color: #361999;
+  border: 2px solid #361999;
+  &:hover {
+    background: #361999;
+    color: #78fff1;
+    border: 2px solid #78fff1;
+    transform: scale(1.2);
+  }
 `;
 
 const ContactsContainer = styled.div`
-    position: relative;
-    top: ${props => props.mobileSite ? '100px' : '200px'};
-    margin: 0 auto;
-    width: 70vw;
-    display: flex;
-    flex-direction: ${props => props.mobileSite ? 'column' : 'row'};
-    justify-content: space-evenly;
-    align-items: center;
+  position: relative;
+  top: ${(props) => (props.mobileSite ? "100px" : "200px")};
+  margin: 0 auto;
+  width: 70vw;
+  display: flex;
+  flex-direction: ${(props) => (props.mobileSite ? "column" : "row")};
+  justify-content: space-evenly;
+  align-items: center;
 `;
 
-const Contacts = ({mobile}) => {
-    return (
-        <ContactsContainer mobileSite={mobile}>
-            <a href="https://github.com/zerico007" style={{animation: 'enterLeft 1s'}}>
-            <ContactBox>
-                <GitHub size={40}/>   
-            </ContactBox>
-            </a> 
-            <a href="https://www.linkedin.com/in/bavin-edwards-777b23180/" style={{animation: 'enterBottom 1s'}}>
-            <ContactBox>
-                <Linkedin size={40}/>
-            </ContactBox>
-            </a>
-            <a href="mailto:bavin_edwards@live.com" style={{animation: 'enterRight 1s'}}>
-            <ContactBox>   
-                <Inbox size={40}/>
-            </ContactBox>
-            </a>
-        </ContactsContainer>
-    )
-}
+const Contacts = ({ mobile }) => {
+  return (
+    <ContactsContainer mobileSite={mobile}>
+      <a
+        href="https://github.com/zerico007"
+        target="_blank"
+        rel="noreferrer"
+        style={{ animation: "enterLeft 1s" }}
+      >
+        <ContactBox>
+          <GitHub size={40} />
+        </ContactBox>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/bavin-edwards-777b23180/"
+        target="_blank"
+        rel="noreferrer"
+        style={{ animation: "enterBottom 1s" }}
+      >
+        <ContactBox>
+          <Linkedin size={40} />
+        </ContactBox>
+      </a>
+      <a
+        href="mailto:bavin_edwards@live.com"
+        target="_blank"
+        rel="noreferrer"
+        style={{ animation: "enterRight 1s" }}
+      >
+        <ContactBox>
+          <Inbox size={40} />
+        </ContactBox>
+      </a>
+    </ContactsContainer>
+  );
+};
 
 export default Contacts;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,139 +1,162 @@
-import React, { useState } from 'react';
-import styled from '@emotion/styled';
-import appleShopPic from '../assets/apple-shop.jpg';
-import auctionsPic from '../assets/auctions.jpg';
-import gradebookPic from '../assets/gradebook.jpg';
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+import appleShopPic from "../assets/apple-shop.jpg";
+import auctionsPic from "../assets/auctions.jpg";
+import gradebookPic from "../assets/gradebook.jpg";
 
 const ProjectDiv = styled.div`
-    position: relative;
-    top: 100px;
-    display: grid;
-    grid-template-areas:
-    'image'
-    'desc';
-    grid-template-rows: 1fr 1fr;
-    margin: 50px auto 0 auto;
-    width: 70vw;
-    max-width: 830px;
-    height: ${props => props.mobileSite ? 'auto' : '600px'};
-    border: none;
-    padding: 10px;
-    border-radius: 6px;
-    box-shadow: 0.3rem 0.3rem 0.3rem 0.5rem lightgrey;
-    animation: enterTop 1.8s;
+  position: relative;
+  top: 100px;
+  display: grid;
+  grid-template-areas:
+    "image"
+    "desc";
+  grid-template-rows: 1fr 1fr;
+  margin: 50px auto 0 auto;
+  width: 70vw;
+  max-width: 830px;
+  height: ${(props) => (props.mobileSite ? "auto" : "600px")};
+  border: none;
+  padding: 10px;
+  border-radius: 6px;
+  box-shadow: 0.3rem 0.3rem 0.3rem 0.5rem lightgrey;
+  animation: enterTop 1.8s;
 `;
 
 const LaunchCover = styled.div`
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    display: ${props => props.launch ? 'flex' : 'none'};
-    z-index: 9;
-    background-color: rgba(0,0,0, 0.8);
-    border-radius: 6px;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  display: ${(props) => (props.launch ? "flex" : "none")};
+  z-index: 9;
+  background-color: rgba(0, 0, 0, 0.8);
+  border-radius: 6px;
 `;
 
 const Description = styled.p`
-    align-self: center;
-    text-align: center;
-    font-weight: bold;
-    line-height: 1.5rem;
-    grid-area: desc;
+  align-self: center;
+  text-align: center;
+  font-weight: bold;
+  line-height: 1.5rem;
+  grid-area: desc;
 `;
 
 const LaunchButton = styled.button`
-    position: relative;
-    border: 2px solid white;
-    outline: none;
-    cursor: pointer;
-    border-radius: 4px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: white;
-    background: none;
-    height: 32px;
-    width: 150px;
-    font-weight: bold;
-    transition: all ease-in-out 0.3s;
-    &:hover {
-        transform: scale(1.3);
-        background: white;
-        color: black;
-    }
+  position: relative;
+  border: 2px solid white;
+  outline: none;
+  cursor: pointer;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  background: none;
+  height: 32px;
+  width: 150px;
+  font-weight: bold;
+  transition: all ease-in-out 0.3s;
+  &:hover {
+    transform: scale(1.3);
+    background: white;
+    color: black;
+  }
 `;
 
-const Projects = ({mobile}) => {
+const Projects = ({ mobile }) => {
+  const [shop, setShop] = useState(false);
+  const [auction, setAuction] = useState(false);
+  const [gradebook, setGradebook] = useState(false);
 
-    const [ shop, setShop ] = useState(false);
-    const [ auction, setAuction ] = useState(false);
-    const [ gradebook, setGradebook ] = useState(false);
+  const imageStyle = {
+    width: "70vw",
+    maxWidth: "830px",
+    cursor: "pointer",
+    gridArea: "image",
+  };
 
-    const imageStyle = {
-        width: '70vw',
-        maxWidth: '830px',
-        cursor: 'pointer',
-        gridArea: 'image'
-    }
+  return (
+    <>
+      <ProjectDiv
+        onMouseEnter={() => setShop(true)}
+        onMouseLeave={() => setShop(false)}
+        mobileSite={mobile}
+      >
+        <img src={appleShopPic} alt="apple-shop-pic" style={imageStyle} />
 
-    return (
-        <>
-        <ProjectDiv 
-            onMouseEnter={() => setShop(true)} 
-            onMouseLeave={() => setShop(false)} 
-            mobileSite={mobile}>
-            
-            <img src={appleShopPic} alt="apple-shop-pic" style={imageStyle}/>
-            
-            <Description>
-                Web app for shopping apple products. Frontend built with React. Backend 
-                built with Node (includes REST APIs, jwt authentication, refresh tokens) and MongoDB
-            </Description>
-            <LaunchCover launch={shop}>
-            <a href="https://zerico007.github.io/apple_shop/">
-                <LaunchButton>Visit</LaunchButton>
-            </a>
-            </LaunchCover>
-        </ProjectDiv>
-        <ProjectDiv 
-            onMouseEnter={() => setAuction(true)} 
-            onMouseLeave={() => setAuction(false)} 
-            mobileSite={mobile}>
-            
-            <img src={auctionsPic} alt="auctions-pic" loading="lazy" style={imageStyle}/>
-            
-            <Description>
-                Web app similar to ebay for posting items for sale and bidding. Built with Python using Django.
-                SQL DB used. 
-            </Description>
-            <LaunchCover launch={auction}>
-            <a href="http://zerico007.pythonanywhere.com/">
-                <LaunchButton>Visit</LaunchButton>
-            </a>
-            </LaunchCover>
-        </ProjectDiv>
-        <ProjectDiv 
-            onMouseEnter={() => setGradebook(true)} 
-            onMouseLeave={() => setGradebook(false)}  
-            mobileSite={mobile} 
-            style={{marginBottom: '100px'}}
-        >
-            <img src={gradebookPic} alt="gradebook-pic" loading="lazy" style={imageStyle}/>
+        <Description>
+          Web app for shopping apple products. Frontend built with React.
+          Backend built with Node (includes REST APIs, jwt authentication,
+          refresh tokens) and MongoDB
+        </Description>
+        <LaunchCover launch={shop}>
+          <a
+            href="https://zerico007.github.io/apple_shop/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <LaunchButton>Visit</LaunchButton>
+          </a>
+        </LaunchCover>
+      </ProjectDiv>
+      <ProjectDiv
+        onMouseEnter={() => setAuction(true)}
+        onMouseLeave={() => setAuction(false)}
+        mobileSite={mobile}
+      >
+        <img
+          src={auctionsPic}
+          alt="auctions-pic"
+          loading="lazy"
+          style={imageStyle}
+        />
 
-            <Description>
-                Web app for recording student grades. Built with Python using Flask. SQL DB used. 
-            </Description>
-            <LaunchCover launch={gradebook}>
-            <a href="http://kabash.pythonanywhere.com/">
-                <LaunchButton>Visit</LaunchButton>
-            </a>
-            </LaunchCover>
-        </ProjectDiv>
-        </>
-    )
-}
+        <Description>
+          Web app similar to ebay for posting items for sale and bidding. Built
+          with Python using Django. SQL DB used.
+        </Description>
+        <LaunchCover launch={auction}>
+          <a
+            href="http://zerico007.pythonanywhere.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <LaunchButton>Visit</LaunchButton>
+          </a>
+        </LaunchCover>
+      </ProjectDiv>
+      <ProjectDiv
+        onMouseEnter={() => setGradebook(true)}
+        onMouseLeave={() => setGradebook(false)}
+        mobileSite={mobile}
+        style={{ marginBottom: "100px" }}
+      >
+        <img
+          src={gradebookPic}
+          alt="gradebook-pic"
+          loading="lazy"
+          style={imageStyle}
+        />
+
+        <Description>
+          Web app for recording student grades. Built with Python using Flask.
+          SQL DB used.
+        </Description>
+        <LaunchCover launch={gradebook}>
+          <a
+            href="http://kabash.pythonanywhere.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <LaunchButton>Visit</LaunchButton>
+          </a>
+        </LaunchCover>
+      </ProjectDiv>
+    </>
+  );
+};
 
 export default Projects;
