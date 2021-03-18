fab61bfc84ae8b0465e87619e89f7f777b8cad76
Bavin Edwards <65621465+zerico007@users.noreply.github.com>
2021-03-17 19:57:27 -0500

    added hover button for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import appleShopPic from '../assets/apple-shop.jpg';
 import auctionsPic from '../assets/auctions.jpg';
@@ -23,6 +23,19 @@ const ProjectDiv = styled.div`
     animation: enterTop 1.8s;
 `;
 
+const LaunchCover = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    display: ${props => props.launch ? 'flex' : 'none'};
+    z-index: 9;
+    background-color: rgba(0,0,0, 0.8);
+    border-radius: 6px;
+`;
+
 const Description = styled.p`
     align-self: center;
     text-align: center;
@@ -31,7 +44,33 @@ const Description = styled.p`
     grid-area: desc;
 `;
 
+const LaunchButton = styled.button`
+    position: relative;
+    border: 2px solid white;
+    outline: none;
+    cursor: pointer;
+    border-radius: 4px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: white;
+    background: none;
+    height: 32px;
+    width: 150px;
+    font-weight: bold;
+    transition: all ease-in-out 0.3s;
+    &:hover {
+        transform: scale(1.3);
+        background: white;
+        color: black;
+    }
+`;
+
 const Projects = ({mobile}) => {
+
+    const [ shop, setShop ] = useState(false);
+    const [ auction, setAuction ] = useState(false);
+    const [ gradebook, setGradebook ] = useState(false);
 
     const imageStyle = {
         width: '70vw',
@@ -42,31 +81,56 @@ const Projects = ({mobile}) => {
 
     return (
         <>
-        <ProjectDiv mobileSite={mobile}>
-            <a href="https://zerico007.github.io/apple_shop/">
+        <ProjectDiv 
+            onMouseEnter={() => setShop(true)} 
+            onMouseLeave={() => setShop(false)} 
+            mobileSite={mobile}>
+            
             <img src={appleShopPic} alt="apple-shop-pic" style={imageStyle}/>
-            </a>
+            
             <Description>
                 Web app for shopping apple products. Frontend built with React. Backend 
                 built with Node (includes REST APIs, jwt authentication, refresh tokens) and MongoDB
             </Description>
-        </ProjectDiv>
-        <ProjectDiv mobileSite={mobile}>
-            <a href="http://zerico007.pythonanywhere.com/">
-            <img src={auctionsPic} alt="auctions-pic" loading="lazy" style={imageStyle}/>
+            <LaunchCover launch={shop}>
+            <a href="https://zerico007.github.io/apple_shop/">
+                <LaunchButton>Visit</LaunchButton>
             </a>
+            </LaunchCover>
+        </ProjectDiv>
+        <ProjectDiv 
+            onMouseEnter={() => setAuction(true)} 
+            onMouseLeave={() => setAuction(false)} 
+            mobileSite={mobile}>
+            
+            <img src={auctionsPic} alt="auctions-pic" loading="lazy" style={imageStyle}/>
+            
             <Description>
                 Web app similar to ebay for posting items for sale and bidding. Built with Python using Django.
                 SQL DB used. 
             </Description>
-        </ProjectDiv>
-        <ProjectDiv mobileSite={mobile} style={{marginBottom: '100px'}}>
-            <a href="http://kabash.pythonanywhere.com/">
-            <img src={gradebookPic} alt="gradebook-pic" loading="lazy" style={imageStyle}/>
+            <LaunchCover launch={auction}>
+            <a href="http://zerico007.pythonanywhere.com/">
+                <LaunchButton>Visit</LaunchButton>
             </a>
+            </LaunchCover>
+        </ProjectDiv>
+        <ProjectDiv 
+            onMouseEnter={() => setGradebook(true)} 
+            onMouseLeave={() => setGradebook(false)}  
+            mobileSite={mobile} 
+            style={{marginBottom: '100px'}}
+        >
+            <img src={gradebookPic} alt="gradebook-pic" loading="lazy" style={imageStyle}/>
+
             <Description>
                 Web app for recording student grades. Built with Python using Flask. SQL DB used. 
             </Description>
+            <LaunchCover launch={gradebook}>
+            <a href="http://kabash.pythonanywhere.com/">
+                <LaunchButton>Visit</LaunchButton>
+            </a>
+            </LaunchCover>
         </ProjectDiv>
         </>
     )
